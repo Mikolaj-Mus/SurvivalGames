f546e04aebf7e1f29b56c806c6678652a9e18c37
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-08-02 09:06:09 +0200

    Made random move for champs and improved mechanics.

--- src/main/java/com/example/survivalgames/Champ.java
+++ src/main/java/com/example/survivalgames/Champ.java
@@ -1,4 +1,8 @@
 package com.example.survivalgames;
+import java.util.concurrent.ThreadLocalRandom;
+
+import java.awt.*;
+import java.util.Random;
 
 public class Champ {
 
@@ -6,12 +10,36 @@ public class Champ {
     private int xCor;
     private int yCor;
     private int strength = 1;
-    private char direction = 'R';
+    private int direction;
 
     public Champ(int id, int x, int y) {
         this.id = id;
         this.xCor = x;
         this.yCor = y;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.ORANGE);
+        g.fillOval(xCor, yCor, Mechanics.returnUNIT_SIZE(), Mechanics.returnUNIT_SIZE());
+    }
+
+    public void move() {
+        direction = ThreadLocalRandom.current().nextInt(0, 4);
+
+        switch (direction) {
+            case 0:
+                yCor -= Mechanics.returnUNIT_SIZE();
+                break;
+            case 1:
+                yCor += Mechanics.returnUNIT_SIZE();
+                break;
+            case 2:
+                xCor -= Mechanics.returnUNIT_SIZE();
+                break;
+            case 3:
+                xCor += Mechanics.returnUNIT_SIZE();
+                break;
+        }
     }
 
     public int getId() {
--- src/main/java/com/example/survivalgames/Mechanics.java
+++ src/main/java/com/example/survivalgames/Mechanics.java
@@ -5,75 +5,74 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Mechanics extends JPanel implements ActionListener {
 
     private static final int SCREEN_WIDTH = 600;
     private static final int SCREEN_HEIGHT = 600;
-    private static final int UNIT_SIZE = 60;
+    private static final int UNIT_SIZE = 5;
     private static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
-    private static final int DELAY = 1000;
+    private static final int DELAY = 10;
     private Champ[] champTab;
     boolean running = false;
     Timer timer;
     Random random;
 
 
-    Mechanics(){
-        random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+    Mechanics() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
-//        this.addKeyListener(new MyKeyAdapter());
-        createChamps(12);
+        createChamps(1000);
         startGame();
     }
 
     public void startGame() {
-
         running = true;
-        timer = new Timer(DELAY,this);
+        timer = new Timer(DELAY, this);
         timer.start();
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        draw(g);
+        drawLines(g);
+        for (Champ champ : champTab) {
+            champ.draw(g);
+        }
     }
-    public void draw(Graphics g) {
 
-        if(running) {
+    public void drawLines(Graphics g) {
+        if (running) {
             for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
             }
-            for (int i = 0; i < champTab.length; i++) {
-                g.setColor(Color.ORANGE);
-                g.drawOval(champTab[i].getxCor(), champTab[i].getyCor(),UNIT_SIZE,UNIT_SIZE);
-            }
-
         }
     }
 
     public void createChamps(int number) {
-        int index = 0;
         champTab = new Champ[number];
-        champTab[0] = new Champ(0, 0, 0);
-//        for (int i = 0; i < SCREEN_WIDTH/UNIT_SIZE; i+=3) {
-//            for (int j = 0; j < SCREEN_HEIGHT/UNIT_SIZE; j+=3) {
-//                champTab[index] = new Champ(index, i, j);
-//                index++;
-//            }
-//
-//        }
+        for (int i = 0; i < champTab.length; i++) {
+            int x = ThreadLocalRandom.current().nextInt(0, (int)Math.sqrt(GAME_UNITS));
+            int y = ThreadLocalRandom.current().nextInt(0, (int)Math.sqrt(GAME_UNITS));
+            champTab[i] = new Champ(0,x*UNIT_SIZE,y*UNIT_SIZE);
+        }
     }
 
     public static int returnGAME_UNITS() {
         return GAME_UNITS;
     }
+    public static int returnUNIT_SIZE() {
+        return UNIT_SIZE;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        for (Champ champ : champTab) {
+            champ.move();
+        }
+        repaint();
     }
 
 
